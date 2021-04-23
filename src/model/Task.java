4a3779b2494ca59a0ee8762074c9f7bdd570c46f
kbhat23 <70919412+kbhat23@users.noreply.github.com>
2021-04-23 14:39:41 -0700

    Fixed errors pertaining to packages declarations and imports

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,4 +1,5 @@
-package TodoTask;
+package model;
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -8,8 +9,12 @@ import java.util.Date;
  * Class that represents a single task for a user to add, edit, complete. 
  *
  */
-public class Task {
+public class Task implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8426466080288612855L;
 	String taskName, description, category, location; 
 	int priority; 
 	boolean completed; 
