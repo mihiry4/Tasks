1ded3d988fc2193f456c8d9fcdc93034e331e57c
kbhat23 <70919412+kbhat23@users.noreply.github.com>
2021-04-24 21:41:08 -0700

    Wrote tests in TodoTestClass to 100% branch coverage for TaskList class.

--- src/model/TaskList.java
+++ src/model/TaskList.java
@@ -89,51 +89,6 @@ public class TaskList implements Serializable {
 	}
 	
 	/**
-	 * Gets the task at given index - useful for iterating over TaskList.
-	 * @param index the index at which to grab the task.
-	 * @return the Task at the given index.
-	 * @throws IndexOutOfBoundsException if index not valid for the number of tasks.
-	 */
-	public Task getTask(int index) {
-		
-		if (index < taskList.size() && index >= 0) {
-			return taskList.get(index);
-		}
-		throw new IndexOutOfBoundsException();
-		
-	}
-	
-	/**
-	 * Returns the number of tasks in the task list.
-	 * @return the number of tasks in the tas list.
-	 */
-	public int size() {
-		return taskList.size();
-	}
-	
-	
-	/**
-	 * Modifies a task given the dateCreated of the task and all of its new values.
-	 * Modifies a task given the task to be modified and all of its new values; values 
-	 * that should be kept the same should be passed in as the old value.
-	 * @param dateCreated the dateCreated of the task to be modified.
-	 * @param taskName the new name of the task.
-	 * @param description the new description of the task.
-	 * @param priority the new priority of the task.
-	 * @param category the new category of the task.
-	 * @param completed the new completedness of the task.
-	 * @param dateDue the new due date of the task.
-	 * @param location the new location of the task.
-	 */
-	public void modifyTask(Date dateCreated, String taskName, String description, int priority, String category,
-			boolean completed, Date dateDue, String location) {
-		
-		Task task = getTask(dateCreated);
-		modifyTask(task, taskName, description, priority, category, completed, dateDue, location);
-	}
-	
-	
-	/**
 	 * Modifies a task given the task to be modified and all of its new values.
 	 * Modifies a task given the task to be modified and all of its new values; values 
 	 * that should be kept the same should be passed in as the old value.
@@ -157,5 +112,55 @@ public class TaskList implements Serializable {
 		task.setDateDue(dateDue);
 		task.setLocation(location);
 	}
+
+	
+//USELESS METHODS
+//	
+//	/**
+//	 * Gets the task at given index - useful for iterating over TaskList.
+//	 * @param index the index at which to grab the task.
+//	 * @return the Task at the given index.
+//	 * @throws IndexOutOfBoundsException if index not valid for the number of tasks.
+//	 */
+//	public Task getTask(int index) {
+//		
+//		if (index < taskList.size() && index >= 0) {
+//			return taskList.get(index);
+//		}
+//		throw new IndexOutOfBoundsException();
+//		
+//	}
+//	
+//	
+//	/**
+//	 * Returns the number of tasks in the task list.
+//	 * @return the number of tasks in the tas list.
+//	 */
+//	public int size() {
+//		return taskList.size();
+//	}
+//	
+//	
+//	/**
+//	 * Modifies a task given the dateCreated of the task and all of its new values.
+//	 * Modifies a task given the task to be modified and all of its new values; values 
+//	 * that should be kept the same should be passed in as the old value.
+//	 * @param dateCreated the dateCreated of the task to be modified.
+//	 * @param taskName the new name of the task.
+//	 * @param description the new description of the task.
+//	 * @param priority the new priority of the task.
+//	 * @param category the new category of the task.
+//	 * @param completed the new completedness of the task.
+//	 * @param dateDue the new due date of the task.
+//	 * @param location the new location of the task.
+//	 */
+//	public void modifyTask(Date dateCreated, String taskName, String description, int priority, String category,
+//			boolean completed, Date dateDue, String location) {
+//		
+//		Task task = getTask(dateCreated);
+//		modifyTask(task, taskName, description, priority, category, completed, dateDue, location);
+//	}
+	
+
 
 }
