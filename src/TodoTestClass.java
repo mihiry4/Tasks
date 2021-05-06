4357d0627ea5c7776d64969ab9d621016cf55f1b
Shreyas Khandekar <shreyaskhandekar@email.arizona.edu>
2021-05-05 17:08:02 -0700

    Coverage Hotfix

--- src/TodoTestClass.java
+++ src/TodoTestClass.java
@@ -1,6 +1,11 @@
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -399,7 +404,7 @@ class TodoTestClass {
 	void testException() throws TodoDueDateInPastException, TodoEmptyTaskNameException {
 		TodoModel model = new TodoModel(); 
 		TodoController controller = new TodoController(model);
-		Date date = new Date(118,5,4,10,40);
+		Date date = new Date();
 		
 		TodoDueDateInPastException past = new TodoDueDateInPastException("");
 		TodoEmptyTaskNameException noName = new TodoEmptyTaskNameException("Task must have a name");
@@ -421,5 +426,50 @@ class TodoTestClass {
 	}
 	
 	
+<<<<<<< Updated upstream
 
+=======
+	/*
+	 * Test method for TodoModel#getTask(Date)
+	 * Test method for TodoModel#getTaskListSize()
+	 * Test method for TodoModel#removeTask(Date)
+	 */
+	@Test
+	void testModelGetters() {
+		TodoModel model = new TodoModel();
+		
+		Task randomTask = new Task("Task", "New Task", 0, "", new Date(), "Tucson");
+		Date dateCreated = randomTask.getDateCreated();
+		model.addTask(randomTask);
+		
+		// First check that task was added correctly. 
+		assertEquals(model.getTask(dateCreated), randomTask);
+		
+		// Now check that the removed task is the same as the first task 
+		// we created. 
+		assertEquals(model.removeTask(dateCreated), randomTask);
+		
+		// Check for empty task List size
+		assertEquals(model.getTaskListSize(), 0);
+	}
+	
+	@Test
+	void todoModelSaveTest() {
+		File file = new File("example.dat");
+		TodoModel model = new TodoModel(file);
+		TaskList tl = new TaskList(model.getTaskList());
+		model = new TodoModel(tl);
+		FileOutputStream fout;
+		try {
+			fout = new FileOutputStream("test.dat");
+			ObjectOutputStream oos = new ObjectOutputStream(fout);
+			model.saveList(oos);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+>>>>>>> Stashed changes
 }
